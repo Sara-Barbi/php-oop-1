6532319c523656b80fbf348d2d708474182eacfa
Sara-Barbi <barbisara02@gmail.com>
2022-02-25 22:00:35 +0100

    altri 2 film

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ $cinema = [
     new Movie("Winnie the Pooh","un'emozionante storia piena di Miele!","Cartone animato,per bambini"),
     new Movie("1917","un'emozionante documentario colmo di eventi storici da non perdere!","Documentario,per adulti"),
     new Movie("i due Papi","In un momento di svolta per la Chiesa cattolica, Papa Benedetto XVI stringe una sorprendente amicizia con il futuro Papa Francesco. Ispirato a eventi reali.","Drammatico,per aldulti"),
-    new Movie("After Party","un emozionante storia piena di Mojito!","Divertente,per adulti e bambini accompagnati")
+    new Movie("After Party","un emozionante storia piena di Mojito!","Divertente,per adulti e bambini accompagnati"),
+    new Movie("Mamma ho perso l'Aereo","un film visto e rivisto, un classico che non tradisce mai","Drammatico,comico,per aldulti e bambini"),
+    new Movie("Titanic","un emozionante storia piena di colpi di scena, basato su una storia vera","Drammatico,per adulti e bambini accompagnati")
 ];
 
 $cinema[1]-> setAttori("George MacKay: caporale William Schofield, Dean-Charles Chapman: caporale Tom Blake, Mark Strong: capitano Smith, Andrew Scott: tenente Leslie, Richard Madden: tenente Joseph Blake, Claire Duburcq: Lauri, Colin Firth: generale Erinmore, Benedict Cumberbatch : colonnello Mackenzie");
